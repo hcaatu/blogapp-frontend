89245541f5a718e2d024f00348124d8947b234aa
hcaatu <aatu.hokkane@gmail.com>
2025-02-14 11:29:07 +0200

    tests initialized

---
+++ src/components/Blog.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import Blog from './Blog'
+
+test('renders title', () => {
+  const blog = {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+  }
+})
+
+
